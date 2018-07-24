2c4fcd12af5352d73b7fd2a3da97977a143ca409
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-07-23 22:16:11 -0300

    removendo imports não usados

--- app/src/main/java/mmalaquiasdev/github/com/agenda/MainActivity.java
+++ app/src/main/java/mmalaquiasdev/github/com/agenda/MainActivity.java
@@ -1,8 +1,7 @@
 package mmalaquiasdev.github.com.agenda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
