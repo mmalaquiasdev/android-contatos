195fa875cf443af127c74b3497ca8e67e59644ae
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-07-24 10:27:28 -0300

    Nossa fiz muitas alterações aqui, acabei me perdendo nos commits, mas praticamente foi tudo para refatorar o botão salvar que foi ficar no menu da pagina de formulario

--- app/src/main/java/mmalaquiasdev/github/com/agenda/FormularioActivity.java
+++ app/src/main/java/mmalaquiasdev/github/com/agenda/FormularioActivity.java
@@ -2,6 +2,8 @@ package mmalaquiasdev.github.com.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,14 +15,27 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_formulario);
-        this.acaoBotaoSalvar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_ok:
+                acaoBotaoSalvar();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void acaoBotaoSalvar() {
-        Button botaoSalvar = findViewById(R.id.formulario_bt_salvar);
-        botaoSalvar.setOnClickListener((view) -> {
-            makeText(FormularioActivity.this, "Contato salvo com sucesso", LENGTH_SHORT).show();
-            finish();
-        });
+        makeText(FormularioActivity.this, "Contato salvo com sucesso", LENGTH_SHORT).show();
+        finish();
     }
 }
