3805f7cb8593f012871744ac922a8ceb5395b4d1
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2018-07-23 22:53:07 -0300

    Adicionado ids nas tags do formulario e um listener para o bt salvar

--- app/src/main/java/mmalaquiasdev/github/com/agenda/FormularioActivity.java
+++ app/src/main/java/mmalaquiasdev/github/com/agenda/FormularioActivity.java
@@ -2,6 +2,10 @@ package mmalaquiasdev.github.com.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.widget.Toast.*;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        acaoBotaoSalvar();
+    }
+
+    private void acaoBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.formulario_bt_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeText(FormularioActivity.this, "Cliquei no botão", LENGTH_SHORT)
+                .show();
+            }
+        });
     }
 }
